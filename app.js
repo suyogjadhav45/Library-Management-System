cd3ba2b9bf45362348d8b207d5322e6a9d3f2f32
manasjadhav <manasjjadhav@gmail.com>
2022-12-05 20:45:36 +0530

    deploy on cyclic

--- app.js
+++ app.js
@@ -31,8 +31,10 @@ mongoose
   })
   .catch((err) => console.error(err));
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.use(express.static("client/build"));
+const path = require("path");
+app.get("*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 app.use(userRoutes);
